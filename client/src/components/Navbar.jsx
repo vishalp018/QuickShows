200d2adb4e285102d7fba0694a1ffa19de768c31
VISHAL <palvishal111@gmail.com>
2025-09-07 12:23:13 +0530

    Added auth using clerk

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
-import { Link } from "react-router-dom";
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
 
   const [isOpen ,setIsOpen]=useState(false);
+  const {user}=useUser();
+  const {openSignIn}=useClerk();
+  const navigate=useNavigate();
+
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
       <Link to='/' className='max-md:flex-1'>
@@ -16,18 +21,28 @@ const Navbar = () => {
       py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border 
       border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
         <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=>setIsOpen(!isOpen)} />
-          <Link onClick={()=>{scrollTo(0,0)}} to='/'>Home</Link>
-          <Link to='/movies'>Movies</Link>
-          <Link to='/'>Theaters</Link>
-          <Link to='/'>Releases</Link>
-          <Link to='/favorite'>Favorites</Link>
+          <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to='/'>Home</Link>
+          <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to='/movies'>Movies</Link>
+          <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to='/'>Theaters</Link>
+          <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to='/'>Releases</Link>
+          <Link onClick={()=>{scrollTo(0,0),setIsOpen(false)}} to='/favorite'>Favorites</Link>
       </div>
       <div className='flex items-center gap-8'>
             <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-            <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull 
-            transition rounded-full font-medium cursor-pointer'>Login</button>
+            {
+              !user?(<button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull 
+            transition rounded-full font-medium cursor-pointer'>Login</button>):(
+              <UserButton > <UserButton.MenuItems>
+                <UserButton.Action label='My Bookings' labelIcon={<TicketPlus width={15}/>}
+                onClick={()=>navigate('/my-bookings')}/>
+                </UserButton.MenuItems>
+                </UserButton>
+            )
+
+            }
+            
       </div>
-      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer"/>
+      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" onClick={()=>setIsOpen(!isOpen)}/>
 
     </div>
   )
